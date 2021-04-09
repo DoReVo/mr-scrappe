4274277f796d8c31bf05aadf15433fcb0c9fb173
DoReVo <frz565@gmail.com>
2021-04-10 00:40:44 +0800

    docs(ts): changed typings for browserService

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,7 +1,14 @@
-import { Browser } from "puppeteer";
+import { Browser, DirectNavigationOptions, Page } from "puppeteer";
 
 export declare namespace Scrape {
   interface Item {
     scrape: (browser: Browser) => void;
+  }
+
+  interface BrowserService {
+    init: () => void;
+    closeBrowser: () => void;
+    getBrowserInstance: () => Browser;
+    createPage(url: string, options: DirectNavigationOptions): Promise<Page>;
   }
 }
