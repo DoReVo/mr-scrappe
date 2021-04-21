9ece18e0a4dcd01b39ee00a0a3f72bccec8d30f9
DoReVo <frz565@gmail.com>
2021-04-22 01:05:16 +0800

    feat(cf-worker): change to return json straight away

--- src/fetch.ts
+++ src/fetch.ts
@@ -4,8 +4,7 @@ const fetchWrapper = async (url: string) => {
   let response
   if (process.env.NODE_ENV === 'development') response = await nodeFetch(url)
   else response = await fetch(url)
-
-  return response
+  return await response.json()
 }
 
 export { fetchWrapper }
